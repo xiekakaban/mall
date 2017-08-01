2b0dabdde9c0220181f1d56eb46f25376791c053
xiekakaban <ruantianbo@163.com>
2017-08-01 21:54:41 +0800

    change to use springboot

---
+++ src/main/java/com/statestr/mall/MallApplication.java
@@ -0,0 +1,19 @@
+package com.statestr.mall;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+
+import java.util.List;
+
+/**
+ * Created by e604845 on 8/1/2017.
+ */
+@SpringBootApplication
+public class MallApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MallApplication.class, args);
+    }
+}
