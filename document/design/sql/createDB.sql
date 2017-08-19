032c5d36d3e2a9e3a101333b7cdb44b233096eb3
ruantianbo <ruantianbo@163.com>
2017-08-19 16:02:47 +0800

    add

--- document/design/sql/createDB.sql
+++ document/design/sql/createDB.sql
@@ -1,3 +1,6 @@
+导入文件：mysql>source 导入的文件名;
+mysqldump -u 用户名 -p 数据库名 > 导出的文件名
+
 CREATE TABLE tb_administrator
 (
     id VARCHAR(50) PRIMARY KEY NOT NULL,
@@ -10,6 +13,8 @@ CREATE TABLE tb_administrator
     create_time DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
 CREATE UNIQUE INDEX username ON tb_administrator (username);
+
+
 
 
 
